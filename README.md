e5aeb70dc0f1d82ae3003b50656f8950c74bef0e
Viktor Dziuban <vsybrex@gmail.com>
2022-04-21 15:22:00 +0300

    Fixes and refactoring

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# fab
-Fabric tasks
+# Fabric tasks
+
+## Deployment
+
+Settings on the remote server
+-----------------------------
+Account deployer with deploy key for github
+https://docs.github.com/en/developers/overview/managing-deploy-keys#deploy-keys
+
+```commandline
+# /home/deployer/.ssh/config
+Host fab.github.com
+HostName github.com
+User git
+IdentityFile ~/.ssh/github_fab
+```
+
+Settings on local computer
+--------------------------
+SSH connection settings for deployer user
+```commandline
+# ~/.ssh/config
+Host myserver-dep
+HostName 1.1.1.1
+User deployer
+IdentityFile ~/.ssh/myserver_deployer_key
+```
--- fabfile.py
+++ fabfile.py
@@ -4,22 +4,22 @@ from fabric.tasks import task
 
 
 load_dotenv()
-PYTHON = environ.get('SERVER_PYTHON_PATH', '/usr/local/bin/python3.8')
+PYTHON = environ.get('SERVER_PYTHON_PATH')
 ACTIVATE_VENV = environ.get('ACTIVATE_VENV', 'source .venv/bin/activate')
-USERNAME = environ.get('USERNAME', 'deployer')
-PROJECTS_PATH = environ.get('PROJECTS_PATH', '/var/www/vhosts')
-PROJECT_NAME = environ.get('PROJECT_NAME', 'fab')
+USERNAME = environ.get('USERNAME')
+PROJECTS_PATH = environ.get('PROJECTS_PATH')
+PROJECT_NAME = environ.get('PROJECT_NAME')
 GIT_REPOSITORY = environ.get('GIT_REPOSITORY')
 
 
 @task
-def install(c, project):
+def install(c):
     """
-    Install project
-    fab install --project=<name> --hosts=<ip>
+    Install project on remote server
+    fab install --hosts=<ip> (IP or record from .ssh/config)
     """
-    c.run(f'mkdir {PROJECTS_PATH}/{project}')
-    with c.cd(f'{PROJECTS_PATH}/{project}'):
+    c.run(f'mkdir {PROJECTS_PATH}/{PROJECT_NAME}')
+    with c.cd(f'{PROJECTS_PATH}/{PROJECT_NAME}'):
         c.run(f'git clone {GIT_REPOSITORY} .')
         c.run(f'{PYTHON} -m venv .venv')
         c.run(f'{ACTIVATE_VENV} && pip install --upgrade pip')
@@ -28,30 +28,30 @@ def install(c, project):
 
 
 @task
-def upload(c, project, local, remote):
+def upload(c, local, remote):
     """
     Upload file
-    fab upload --project=<name> --local=</path/to/local/file> --remote=<relative/path/to/file> --hosts=<ip>
+    fab upload --local=</path/to/local/file> --remote=<relative/path/to/file> --hosts=<ip>
     """
-    c.put(local, f'{PROJECTS_PATH}/{project}/{remote}')
+    c.put(local, f'{PROJECTS_PATH}/{PROJECT_NAME}/{remote}')
 
 
 @task
-def download(c, project, remote, local):
+def download(c, remote, local):
     """
     Download file
-    fab download --project=<name> --remote=<relative/path/to/file> --local=</path/to/local/file> --hosts=<ip>
+    fab download --remote=<relative/path/to/file> --local=</path/to/local/file> --hosts=<ip>
     """
-    c.get(f'{PROJECTS_PATH}/{project}/{remote}', local)
+    c.get(f'{PROJECTS_PATH}/{PROJECT_NAME}/{remote}', local)
 
 
 @task
-def deploy(c, project, branch='master', deps=False):
+def deploy(c, branch='master', deps=False):
     """
     Deploy updates to server
-    fab deploy --project=<name> --branch=<name> --deps --hosts=<ip>
+    fab deploy --branch=<name> --deps --hosts=<ip>
     """
-    with c.cd(f'{PROJECTS_PATH}/{project}'):
+    with c.cd(f'{PROJECTS_PATH}/{PROJECT_NAME}'):
         print('> Updating code')
         c.run(f'git fetch origin && git checkout {branch} && git pull origin {branch}')
         if deps:
